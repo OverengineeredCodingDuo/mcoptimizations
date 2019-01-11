fce05907226d73bae4184835862bdb5d617a679b
PhiPro95 <PProvenzano@web.de>
2019-01-11 21:47:01 +0100

    Replace synchronized AtomicBitArray.setAt with CAS

--- src/main/java/ocd/concurrent/util/AtomicBitArray.java
+++ src/main/java/ocd/concurrent/util/AtomicBitArray.java
@@ -7,65 +7,99 @@ import org.apache.commons.lang3.Validate;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.BitArray;
 import net.minecraft.util.math.MathHelper;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class AtomicBitArray extends BitArray
+public class AtomicBitArray
 {
     private final AtomicLongArray data;
+    private final int bitsPerEntry;
     private final int entriesPerLong;
+    private final long maxEntryValue;
+    private final int arraySize;
 
     public AtomicBitArray(int bitsPerEntryIn, int arraySizeIn)
     {
-        super(bitsPerEntryIn, arraySizeIn);
+        this.bitsPerEntry = bitsPerEntryIn;
         this.entriesPerLong = 64 / bitsPerEntryIn;
+        this.arraySize = arraySizeIn;
+        this.maxEntryValue = (1L << bitsPerEntryIn) - 1;
         this.data = new AtomicLongArray(MathHelper.roundUp(arraySizeIn, this.entriesPerLong) / this.entriesPerLong);
     }
 
     public AtomicBitArray(int bitsPerEntryIn, int arraySizeIn, long[] data)
     {
-        super(bitsPerEntryIn, arraySizeIn, data);
-        this.entriesPerLong = 64 / bitsPerEntryIn;
-        this.data = new AtomicLongArray(MathHelper.roundUp(arraySizeIn, this.entriesPerLong) / this.entriesPerLong);
-        this.sync();
+        this(bitsPerEntryIn, arraySizeIn);
+        this.read(data);
     }
 
-    public synchronized void setAt(int index, int value)
+    public void setAt(int index, int value)
     {
-        super.setAt(index, value);
-        this.setAt_(index, value);
-    }
-
-    private void setAt_(int index, int value)
-    {
-        Validate.inclusiveBetween(0, this.size() - 1, index);
+        Validate.inclusiveBetween(0, this.arraySize - 1, index);
         Validate.inclusiveBetween(0, this.maxEntryValue, value);
+
         final int i = index / this.entriesPerLong;
-        final int shift = (index - i * this.entriesPerLong) * this.bitsPerEntry();
-        this.data.lazySet(i, (this.data.get(i) & ~(this.maxEntryValue << shift)) | ((value & this.maxEntryValue) << shift));
+        final int shift = (index - i * this.entriesPerLong) * this.bitsPerEntry;
+
+        long oldVal;
+        do
+        {
+            oldVal = this.data.get(i);
+        } while (!this.data.compareAndSet(i, oldVal, (oldVal & ~(this.maxEntryValue << shift)) | (((long) value) << shift)));
     }
 
     public int getAt(int index)
     {
-        Validate.inclusiveBetween(0, this.size() - 1, index);
+        Validate.inclusiveBetween(0, this.arraySize - 1, index);
         final int i = index / this.entriesPerLong;
-        final int shift = (index - i * this.entriesPerLong) * this.bitsPerEntry();
+        final int shift = (index - i * this.entriesPerLong) * this.bitsPerEntry;
         return (int) ((this.data.get(i) >>> shift) & this.maxEntryValue);
     }
 
-    public synchronized void sync()
+    public long[] getSerializedLongArray()
     {
-        for (int i = 0; i < this.size(); ++i)
-            this.setAt_(i, super.getAt(i));
+        final BitArray bitArray = new BitArray(this.bitsPerEntry, this.arraySize);
+
+        for (int i = 0; i < this.arraySize; ++i)
+            bitArray.setAt(i, this.getAt(i));
+
+        return bitArray.getBackingLongArray();
     }
 
+    public int serializedSize()
+    {
+        return MathHelper.roundUp(this.arraySize * this.bitsPerEntry, 64) / 64;
+    }
+
+    public int size()
+    {
+        return this.arraySize;
+    }
+
+    public int bitsPerEntry()
+    {
+        return this.bitsPerEntry;
+    }
+
+    public void read(final BitArray bitArray)
+    {
+        if (this.arraySize != bitArray.size())
+            throw new IllegalArgumentException(String.format("Trying to read array of size %s into array of incompatible size %s", bitArray.size(), this.arraySize));
+
+        for (int i = 0; i < this.arraySize; ++i)
+            this.setAt(i, bitArray.getAt(i));
+    }
+
+    public void read(final long[] data)
+    {
+        this.read(new BitArray(this.bitsPerEntry, this.arraySize, data));
+    }
+
+    @OnlyIn(Dist.CLIENT)
     public synchronized void read(PacketBuffer buf)
     {
-        buf.readLongArray(this.getBackingLongArray());
-        this.sync();
-    }
-
-    public synchronized void read(final long[] data)
-    {
-        System.arraycopy(data, 0, this.getBackingLongArray(), 0, data.length);
-        this.sync();
+        final BitArray bitArray = new BitArray(this.bitsPerEntry, this.arraySize);
+        buf.readLongArray(bitArray.getBackingLongArray());
+        this.read(bitArray);
     }
 }
